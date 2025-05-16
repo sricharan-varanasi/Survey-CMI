1789053d5b25aeaf50391ed15a502eced5e23c33
sricharan-varanasi <sricharanvaranasi2000@gmail.com>
2025-05-16 17:19:27 -0400

    Bug changes

--- backend/app/routers/subscales.py
+++ backend/app/routers/subscales.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import models, schemas
